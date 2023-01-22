13a37f298ab07b5cb7be0fc68c16f26bc9ac1889
upendra pal <06072000@gmail.com>
2023-01-22 22:53:46 +0530

    update

--- Javascript/MobileOtp.js
+++ Javascript/MobileOtp.js
@@ -2,21 +2,19 @@ let sowcolor = document.getElementById('input')
 let inputvalue = document.getElementById('numberInput')
 let heading = document.getElementById('heading')
 let btn = document.getElementById('btn')
-let contact=document.getElementById('nomb')
+let contact = document.getElementById('nomb')
 
-let datalist=JSON.parse(localStorage.getItem('cantact-number'))||[]
+let datalist = JSON.parse(localStorage.getItem('cantact-number')) || []
 sowdataon(datalist)
 function sowdataon(data) {
-for(let i=0;i<data.length;i++){
-    
-contact.innerHTML=`OTP was sent to +91${data[i]}`
-delete datalist[i]
+    for (let i = 0; i < data.length; i++) {
+
+        contact.innerHTML = `OTP was sent to +91${data[i]}`
+        delete datalist[i]
+    }
 }
-}
 
-var randomOtp=Math.floor(Math.random()*1000000)
-
-
+var randomOtp = Math.floor(Math.random() * 1000000)
 
 function changecolor() {
     sowcolor.classList.add('Input')
@@ -25,32 +23,35 @@ function changecolor() {
 inputvalue.addEventListener('input', () => {
 
     let value = inputvalue.value;
-   
-    if (+value== randomOtp&&value.length==6) {
+
+    if (+value == randomOtp && value.length == 6) {
         heading.innerText = ""
         sowcolor.classList.remove('rong')
         btn.classList.add('Btn')
         btn.addEventListener('click', () => {
-            window.location.assign('./Resister.html')
-            
+
+
+
+          
+
         })
     }
     else {
         heading.innerText = 'Enter a valid OTP'
         sowcolor.classList.add('rong')
         btn.classList.remove('Btn')
-      
+
     }
-    
+
 })
 
- 
-   
- window.setTimeout(() => {
+
+
+window.setTimeout(() => {
     alert(`Your OTP is ${randomOtp}`)
 }, 3000);
 
-window.setTimeout(()=>{
+window.setTimeout(() => {
     alert('Your OTP is Expire in 20 Second please enter correct OTP')
-     
-},20000)
+
+}, 20000)
--- Javascript/Profile.js
+++ Javascript/Profile.js
@@ -1,6 +1,7 @@
 
 let Name=document.querySelector('.name')
 let NumberData=document.querySelector('.number')
+let NumberData2=document.querySelector('.mobn')
 let Email=document.querySelector('.email-id')
 let Name2=document.querySelector('.name2')
 let Email2=document.querySelector('.Email')
@@ -12,7 +13,7 @@ let Email2=document.querySelector('.Email')
 let Namelist=JSON.parse(localStorage.getItem('User-Name'))||[]
 let NumberData1=JSON.parse(localStorage.getItem('cantact-number'))||[]
 let EmailList=JSON.parse(localStorage.getItem('Email-data'))||[]
-// console.log(Namelist,NumberData,EmaliList);
+console.log(Namelist,NumberData1,EmailList);
 
 
 sowName(Namelist)
@@ -32,6 +33,7 @@ for(let i=0;i<data2.length;i++){
     
 
 NumberData.innerHTML=`${data2[i]}`
+NumberData2.innerHTML=`${data2[i]}`
 
 delete NumberData1[i]
 }
@@ -39,7 +41,7 @@ delete NumberData1[i]
 
 sowEmail(EmailList)
 function sowEmail(emdata) {
-for(let i=emdata.length-1;i>=0;i--){
+for(let i=0;i<emdata.length;i++){
 Email.innerHTML=`${emdata[i]}`
 Email2.innerHTML=`${emdata[i]}`
 delete EmailList[i]
